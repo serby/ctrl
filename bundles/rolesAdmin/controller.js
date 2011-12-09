08b1505ac04714a28d7880afef5b45eb1bc00518
Paul Serby <paul.serby@clock.co.uk>
2011-12-09 22:15:35 +0000

    Roles admin view now pulls data from adminAccessControl

--- bundles/rolesAdmin/controller.js
+++ bundles/rolesAdmin/controller.js
@@ -3,45 +3,47 @@ var
 	generic = require('../generic'),
 	viewRenderDelegate = require('../../lib/viewRenderDelegate');
 
-var viewSchema = generic.createViewSchema({
-	groups: [{
-		name: 'Role Details',
-		description: 'These are the details for a role',
-		properties: {
-			name: {
-				list: true,
-				view: true,
-				createForm: true,
-				updateForm: true
-			},
-			_id: {
-				form: true,
-				type: 'hidden'
-			},
-			grants: {
-				list: true,
-				view: true,
-				createForm: true,
-				updateForm: true
-			},
-			created: {
-				list: true,
-				view: true,
-				type: 'dateTime'
-			}
-		}
-	}],
-	formPostHelper: function(req, res, next) {
-		var proc = formHelper.processors;
-
-		formHelper.process(req, {
-			visible: proc.boolean
-		});
-
-		next();
-	}
-});
-
 module.exports.createRoutes = function(app, properties, serviceLocator, bundleViewPath) {
+
+	var viewSchema = generic.createViewSchema({
+		groups: [{
+			name: 'Role Details',
+			description: 'These are the details for a role',
+			properties: {
+				name: {
+					list: true,
+					view: true,
+					createForm: true,
+					updateForm: true
+				},
+				_id: {
+					form: true,
+					type: 'hidden'
+				},
+				grants: {
+					list: true,
+					view: true,
+					createForm: true,
+					updateForm: true,
+					type: 'multiselect',
+					createOptions: function(callback) {
+						callback(Object.keys(serviceLocator.adminAccessControlList.acl));
+					}
+				},
+				created: {
+					list: true,
+					view: true,
+					type: 'dateTime'
+				}
+			}
+		}],
+		formPostHelper: function(req, res, next) {
+			var proc = formHelper.processors;
+			formHelper.process(req, {
+				visible: proc.boolean
+			});
+			next();
+		}
+	});
 	generic.createRoutes(app, generic.createViewRender('../../admin/views/layout'), viewSchema, serviceLocator.roleModel, null, serviceLocator);
 };
