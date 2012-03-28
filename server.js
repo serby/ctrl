ab8d60845b8c877949c1a3cd01fc72e4899fceda
Paul Serby <paul.serby@clock.co.uk>
2012-03-28 21:27:13 +0100

    tab to spaces

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 var
-	properties = require('./properties').getProperties(),
-	serviceLocator = require('service-locator').createServiceLocator(),
-	nodemailer = require('nodemailer'),
-	databaseAdaptor = require('./lib/database').createDatabaseAdaptor(properties, serviceLocator),
-	sessionDatabaseAdaptor = require('./lib/database').createDatabaseAdaptor(properties, serviceLocator),
-	Application = require('./lib/expressApplication'),
-	bundled,
-	app,
-	globalViewHelpers = require('./viewHelpers/global');
+  properties = require('./properties').getProperties(),
+  serviceLocator = require('service-locator').createServiceLocator(),
+  nodemailer = require('nodemailer'),
+  databaseAdaptor = require('./lib/database').createDatabaseAdaptor(properties, serviceLocator),
+  sessionDatabaseAdaptor = require('./lib/database').createDatabaseAdaptor(properties, serviceLocator),
+  Application = require('./lib/expressApplication'),
+  bundled,
+  app,
+  globalViewHelpers = require('./viewHelpers/global');
 
 // Register the global services needed by your entire application
 serviceLocator
-	.register('properties', properties)
-	.register('mailer', nodemailer.send_mail)
-	.register('logger', require('./lib/logger').createLogger(properties))
-	.register('uploadDelegate', require('fileupload').createFileUpload(properties.dataPath))
-	.register('bundled', bundled = require('bundled')(serviceLocator, { logger: serviceLocator.logger }));
+  .register('properties', properties)
+  .register('mailer', nodemailer.send_mail)
+  .register('logger', require('./lib/logger').createLogger(properties))
+  .register('uploadDelegate', require('fileupload').createFileUpload(properties.dataPath))
+  .register('bundled', bundled = require('bundled')(serviceLocator, { logger: serviceLocator.logger }));
 
 bundled.addBundles(__dirname + '/bundles/', [
-	'home',
-	'administrator',
-	'admin',
-	'rolesAdmin',
-	'generic',
-	'adminUi',
-	'image'
+  'home',
+  'administrator',
+  'admin',
+  'rolesAdmin',
+  'generic',
+  'adminUi',
+  'image'
 ]);
 
 module.exports = app = Application.createApplication(properties, serviceLocator, sessionDatabaseAdaptor);
@@ -33,27 +33,27 @@ serviceLocator.register('app', app);
 
 databaseAdaptor.createConnection(function(connection) {
 
-	serviceLocator
-		.register('databaseConnections', {
-			main: connection
-	});
+  serviceLocator
+    .register('databaseConnections', {
+      main: connection
+  });
 
-	bundled.initialize();
+  bundled.initialize();
 
-	// Make the bundle manager available to views
-	app.configure(function() {
-		app.dynamicHelpers({
-			bundled: function(req, res) {
-				return bundled;
-			},
-			serviceLocator: function(req, res) {
-				return serviceLocator;
-			}
-		});
-	});
+  // Make the bundle manager available to views
+  app.configure(function() {
+    app.dynamicHelpers({
+      bundled: function(req, res) {
+        return bundled;
+      },
+      serviceLocator: function(req, res) {
+        return serviceLocator;
+      }
+    });
+  });
 
-	// Add helpers
-	globalViewHelpers.createHelpers(serviceLocator, properties, app);
+  // Add helpers
+  globalViewHelpers.createHelpers(serviceLocator, properties, app);
 
-	app.start();
+  app.start();
 });
