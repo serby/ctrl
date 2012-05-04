e1b634b5b038f4ae7b7da968b68b5efdb24a7365
Ben Gourley <bn@grly.me>
2012-05-04 18:14:26 +0100

    Added compact to generic routes, and tidied up source.

--- bundles/generic/lib/genericAdminRoutes.js
+++ bundles/generic/lib/genericAdminRoutes.js
@@ -1,222 +1,298 @@
-var
-	_ = require('underscore'),
-	fs = require('fs'),
-	path = require('path'),
-	async = require('async'),
-	util = require('util'),
-	url = require('url'),
-	httpErrors = require('../../../lib/httpErrorHandler'),
-	SearchQueryBuilder = require('../../../lib/utils/buildSearchQuery'),
-	buildSortOptions = require('../../../lib/utils/buildSortOptions'),
-	Pagination = require('../../../lib/utils/pagination');
+var _ = require('underscore')
+  , fs = require('fs')
+  , path = require('path')
+  , async = require('async')
+  , util = require('util')
+  , url = require('url')
+  , httpErrors = require('../../../lib/httpErrorHandler')
+  , SearchQueryBuilder = require('../../../lib/utils/buildSearchQuery')
+  , buildSortOptions = require('../../../lib/utils/buildSortOptions')
+  , Pagination = require('../../../lib/utils/pagination');
 
-module.exports.createRoutes = function (app, viewRender, adminViewSchema, crudDelegate, serviceLocator, customOptions) {
+module.exports.createRoutes = function (app, render, schema, model, serviceLocator, options) {
 
-	var options = {
-		createValidationSet: '',
-		updateValidationSet: '',
-		createTag: undefined,
-		updateTag: undefined,
-		requiredAccess: crudDelegate.urlName
-	};
+  /*
+   * Adds the common JavaScript
+   * required for admin section
+   */
+  function compactMiddleware() {
+    return serviceLocator.compact.js(['admin-common']);
+  }
 
-	_.extend(options, customOptions);
+  /*
+   * Returns an access checking middleware
+   * based on the required access and the action.
+   */
+  function accessCheck(action) {
+    return serviceLocator.adminAccessControl.requiredAccess(
+      options.requiredAccess,
+      action
+    );
+  }
 
-	var views =  {
-		form: __dirname + '/../views/form',
-		list: __dirname + '/../views/list',
-		view: __dirname + '/../views/view'
-	};
+  var defaults = {
+    createValidationSet: '',
+    updateValidationSet: '',
+    createTag: undefined,
+    updateTag: undefined,
+    requiredAccess: model.urlName
+  };
 
-	_.extend(views, customOptions.views);
+  options = _.extend(defaults, options);
 
-	/**
-	* This ensures errors with properties that are not displayed on the form are shown
-	*/
-	function listUnshownErrors(errors, formType) {
+  var views =  {
+    form: __dirname + '/../views/form',
+    list: __dirname + '/../views/list',
+    view: __dirname + '/../views/view'
+  };
 
-		return Object.keys(errors.errors).filter(function(property) {
-			for (var i = 0; i < adminViewSchema.groups.length; i++) {
-				if ((adminViewSchema.groups[i].properties[property] === undefined) || (!adminViewSchema.groups[i].properties[property][formType])) {
-					return true;
-				}
-			}
-			return false;
-		});
-	}
+  _.extend(views, options.views);
 
-	var
-		searchProperties = SearchQueryBuilder.createSearchProperties(adminViewSchema.groups),
-		buildSearchQuery = SearchQueryBuilder.createSearchQueryBuilder(searchProperties, serviceLocator.logger, crudDelegate.name),
-		paginate = Pagination.createPagination(crudDelegate.count, 10);
+  /**
+  * This ensures errors with properties that are not displayed on the form are shown
+  */
+  function listUnshownErrors(errors, formType) {
 
-	app.get('/admin/' + crudDelegate.urlName, buildSearchQuery, buildSortOptions, paginate,
-		serviceLocator.adminAccessControl.requiredAccess(options.requiredAccess, 'read'), function (req, res) {
+    return Object.keys(errors.errors).filter(function(property) {
+      for (var i = 0; i < schema.groups.length; i++) {
+        if ((schema.groups[i].properties[property] === undefined) || (!schema.groups[i].properties[property][formType])) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
 
-		crudDelegate.find(req.query, _.extend(req.options, req.searchOptions), function (errors, dataSet) {
-			viewRender(req, res, views.list, {
-				viewSchema: adminViewSchema,
-				crudDelegate: crudDelegate,
-				dataSet: dataSet.toArray(),
-				page: {
-					title: crudDelegate.name,
-					name: crudDelegate.name,
-					section: crudDelegate.urlName
-				},
-				url: url.parse(req.url, true).query,
-				searchProperties: searchProperties
-			});
-		});
-	});
+  var searchProperties = SearchQueryBuilder.createSearchProperties(schema.groups)
+    , buildSearchQuery = SearchQueryBuilder.createSearchQueryBuilder(
+        searchProperties,
+        serviceLocator.logger,
+        model.name
+      )
+    , paginate = Pagination.createPagination(model.count, 10);
 
-	// This is the default view schema helpers.
-	// Allows for options to be defined in the view schema as an array or a funciton
-	// and then use in the presentation of the form.
-	function adminViewSchemaHelper(adminViewSchema) {
-		return function(req, res, next) {
-			var fn = [];
-			adminViewSchema.groups.forEach(function(group) {
-				Object.keys(group.properties).forEach(function(key) {
-					if (typeof group.properties[key].createOptions === 'function') {
-						fn.push(function(callback) {
-							group.properties[key].createOptions(function(options) {
-								group.properties[key].options = options;
-								callback();
-							});
-						});
-					}
-				});
-			});
-			async.parallel(fn, function(errors, results) {
-				next();
-			});
-		};
-	}
+  app.get(
+    '/admin/' + model.urlName,
+    compactMiddleware(),
+    buildSearchQuery,
+    buildSortOptions,
+    paginate,
+    accessCheck('read'),
+    function (req, res) {
 
-	function isValidationError(error) {
-		if (error === null) {
-			return false;
-		}
-		return error.name === 'ValidationError' ? true : false;
-	}
+      model.find(
+        req.query,
+        _.extend(req.options, req.searchOptions),
+        function (errors, dataSet) {
+          render(req, res, views.list, {
+            viewSchema: schema,
+            crudDelegate: model,
+            dataSet: dataSet.toArray(),
+            page: {
+              title: model.name,
+              name: model.name,
+              section: model.urlName
+            },
+            url: url.parse(req.url, true).query,
+            searchProperties: searchProperties
+          });
+        }
+      );
+    }
+  );
 
-	function render500(next) {
-		return next('Error saving/updating to database');
-	}
+  // This is the default view schema helpers.
+  // Allows for options to be defined in the view schema as an array or a funciton
+  // and then use in the presentation of the form.
+  function schemaHelper(schema) {
+    return function(req, res, next) {
+      var fn = [];
+      schema.groups.forEach(function(group) {
+        Object.keys(group.properties).forEach(function(key) {
+          if (typeof group.properties[key].createOptions === 'function') {
+            fn.push(function(callback) {
+              group.properties[key].createOptions(function(options) {
+                group.properties[key].options = options;
+                callback();
+              });
+            });
+          }
+        });
+      });
+      async.parallel(fn, function(errors, results) {
+        next();
+      });
+    };
+  }
 
-	app.get('/admin/' + crudDelegate.urlName + '/new',
-		serviceLocator.adminAccessControl.requiredAccess(options.requiredAccess, 'create'),
-		adminViewSchemaHelper(adminViewSchema), function (req, res) {
+  function isValidationError(error) {
+    if (error === null) {
+      return false;
+    }
+    return error.name === 'ValidationError' ? true : false;
+  }
 
-		viewRender(req, res, views.form, {
-			viewSchema: adminViewSchema,
-			crudDelegate: crudDelegate,
-			entity: crudDelegate.entityDelegate.makeDefault(),
-			page: {
-				title: crudDelegate.name,
-				section: crudDelegate.urlName,
-				action: 'create'
-			},
-			formType: 'createForm',
-			errors: {}
-		});
-	});
+  function render500(next) {
+    return next('Error saving/updating to database');
+  }
 
-	app.post('/admin/' + crudDelegate.urlName + '/new',
-		serviceLocator.adminAccessControl.requiredAccess(options.requiredAccess, 'create'),
-		serviceLocator.uploadDelegate,
-		adminViewSchema.formPostHelper,
-		adminViewSchemaHelper(adminViewSchema), function (req, res, next) {
+  app.get(
+    '/admin/' + model.urlName + '/new',
+    compactMiddleware(),
+    accessCheck('create'),
+    schemaHelper(schema),
+    function (req, res) {
 
-		crudDelegate.create(req.body, { validationSet: options.createValidationSet }, function (errors, newEntity) {
-			if (isValidationError(errors)) {
-				viewRender(req, res, views.form, {
-					viewSchema: adminViewSchema,
-					crudDelegate: crudDelegate,
-					entity: newEntity,
-					page: {
-						title: crudDelegate.name,
-						section: crudDelegate.urlName,
-						action: 'create'
-					},
-					formType: 'createForm',
-					errors: errors.errors,
-					unshownErrors: listUnshownErrors(errors, 'createForm')
-				});
-			} else if (errors) {
-				render500(next);
-			} else {
-				res.redirect('/admin/' + crudDelegate.urlName + '/' + newEntity._id);
-			}
-		});
-	});
+      render(req, res, views.form, {
+        viewSchema: schema,
+        crudDelegate: model,
+        entity: model.entityDelegate.makeDefault(),
+        page: {
+          title: model.name,
+          section: model.urlName,
+          action: 'create'
+        },
+        formType: 'createForm',
+        errors: {}
+      });
 
-	app.get('/admin/' + crudDelegate.urlName + '/:id', serviceLocator.adminAccessControl.requiredAccess(options.requiredAccess, 'read'), function (req, res) {
-		crudDelegate.read(req.params.id, function (errors, entity) {
-			viewRender(req, res, views.view, {
-				viewSchema: adminViewSchema,
-				crudDelegate: crudDelegate,
-				entity: entity,
-				page: {
-					title: crudDelegate.name,
-					section: crudDelegate.urlName,
-					action: 'read'
-				}
-			});
-		});
-	});
+    }
+  );
 
-	app.get('/admin/' + crudDelegate.urlName + '/:id/edit', adminViewSchemaHelper(adminViewSchema), serviceLocator.adminAccessControl.requiredAccess(options.requiredAccess, 'update'), function (req, res) {
-		crudDelegate.read(req.params.id, function (errors, entity) {
+  app.post(
+    '/admin/' + model.urlName + '/new',
+    compactMiddleware(),
+    accessCheck('create'),
+    serviceLocator.uploadDelegate,
+    schema.formPostHelper,
+    schemaHelper(schema), function (req, res, next) {
 
-			viewRender(req, res, views.form, {
-				viewSchema: adminViewSchema,
-				crudDelegate: crudDelegate,
-				entity: entity,
-				page: {
-					title: crudDelegate.name,
-					section: crudDelegate.urlName,
-					action: 'update'
-				},
-				formType: 'updateForm',
-				errors: {},
-				unshownErrors: []
-			});
-		});
-	});
+      model.create(
+        req.body,
+        { validationSet: options.createValidationSet },
+        function (errors, newEntity) {
+          if (isValidationError(errors)) {
+            render(req, res, views.form, {
+              viewSchema: schema,
+              crudDelegate: model,
+              entity: newEntity,
+              page: {
+                title: model.name,
+                section: model.urlName,
+                action: 'create'
+              },
+              formType: 'createForm',
+              errors: errors.errors,
+              unshownErrors: listUnshownErrors(errors, 'createForm')
+            });
+          } else if (errors) {
+            render500(next);
+          } else {
+            res.redirect('/admin/' + model.urlName + '/' + newEntity._id);
+          }
+        }
+      );
 
-	app.post('/admin/' + crudDelegate.urlName + '/:id/edit', serviceLocator.uploadDelegate,
-		adminViewSchema.formPostHelper, serviceLocator.adminAccessControl.requiredAccess(options.requiredAccess, 'update'), function (req, res, next) {
-		crudDelegate.update(req.params.id, req.body, { tag: options.updateTag, validationSet: options.updateValidationSet }, function (errors, entity) {
-			if (isValidationError(errors)) {
-				viewRender(req, res, views.form, {
-					viewSchema: adminViewSchema,
-					crudDelegate: crudDelegate,
-					entity: entity,
-					page: {
-						title: crudDelegate.name,
-						section: crudDelegate.urlName,
-						action: 'update'
-					},
-					formType: 'updateForm',
-					errors: errors.errors,
-					unshownErrors: listUnshownErrors(errors, 'updateForm')
-				});
-			} else if (errors) {
-				render500(next);
-			} else {
-				res.redirect('/admin/' + crudDelegate.urlName + '/' + entity._id);
-			}
-		});
-	});
+    }
+  );
 
-	app.get('/admin/' + crudDelegate.urlName + '/:id/delete', serviceLocator.adminAccessControl.requiredAccess(options.requiredAccess, 'delete'), function(req, res, next) {
-		crudDelegate['delete'](req.params.id, function(error) {
-			if (error !== null) {
-				res.send(404);
-			} else {
-				res.redirect('/admin/' + crudDelegate.urlName);
-			}
-		});
-	});
+  app.get(
+    '/admin/' + model.urlName + '/:id',
+    compactMiddleware(),
+    accessCheck('read'), function (req, res) {
+      model.read(
+        req.params.id,
+        function (errors, entity) {
+          render(req, res, views.view, {
+            viewSchema: schema,
+            crudDelegate: model,
+            entity: entity,
+            page: {
+              title: model.name,
+              section: model.urlName,
+              action: 'read'
+            }
+          });
+      });
+  });
+
+  app.get(
+    '/admin/' + model.urlName + '/:id/edit',
+    compactMiddleware(),
+    schemaHelper(schema),
+    accessCheck('update'),
+    function (req, res) {
+      model.read(req.params.id, function (errors, entity) {
+
+        render(req, res, views.form, {
+          viewSchema: schema,
+          crudDelegate: model,
+          entity: entity,
+          page: {
+            title: model.name,
+            section: model.urlName,
+            action: 'update'
+          },
+          formType: 'updateForm',
+          errors: {},
+          unshownErrors: []
+        });
+
+      });
+    }
+  );
+
+  app.post(
+    '/admin/' + model.urlName + '/:id/edit',
+    compactMiddleware(),
+    serviceLocator.uploadDelegate,
+    schema.formPostHelper,
+    accessCheck('update'),
+    function (req, res, next) {
+      model.update(
+        req.params.id,
+        req.body,
+        {
+          tag: options.updateTag,
+          validationSet: options.updateValidationSet
+        },
+        function (errors, entity) {
+          if (isValidationError(errors)) {
+            render(req, res, views.form, {
+              viewSchema: schema,
+              crudDelegate: model,
+              entity: entity,
+              page: {
+                title: model.name,
+                section: model.urlName,
+                action: 'update'
+              },
+              formType: 'updateForm',
+              errors: errors.errors,
+              unshownErrors: listUnshownErrors(errors, 'updateForm')
+            });
+          } else if (errors) {
+            render500(next);
+          } else {
+            res.redirect('/admin/' + model.urlName + '/' + entity._id);
+          }
+        }
+      );
+    }
+  );
+
+  app.get(
+    '/admin/' + model.urlName + '/:id/delete',
+    compactMiddleware(),
+    accessCheck('delete'),
+    function(req, res, next) {
+      model['delete'](req.params.id, function(error) {
+        if (error !== null) {
+          res.send(404);
+        } else {
+          res.redirect('/admin/' + model.urlName);
+        }
+      });
+    }
+  );
 };
