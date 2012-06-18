a22401e248afb25e214d377f4258e6502bd2cd3a
Ben Gourley <bn@grly.me>
2012-06-18 12:41:36 +0100

    Tabs to spaces

--- bundles/article/lib/articleModel.js
+++ bundles/article/lib/articleModel.js
@@ -1,98 +1,98 @@
 var
-	Entity = require('piton-entity'),
-	schema = require('./articleEntitySchema'),
-	MongodbCrudDelegate = require('../../../lib/utils/mongodbCrudDelegate'),
-	path = require('path'),
-	url = require('url'),
-	RSS = require('rss');
+  Entity = require('piton-entity'),
+  schema = require('./articleEntitySchema'),
+  MongodbCrudDelegate = require('../../../lib/utils/mongodbCrudDelegate'),
+  path = require('path'),
+  url = require('url'),
+  RSS = require('rss');
 
 module.exports.createModel = function(properties, serviceLocator) {
 
-	var
-		collection,
-		connection = serviceLocator.databaseConnections.main;
+  var
+    collection,
+    connection = serviceLocator.databaseConnections.main;
 
-	connection.collection('article', function(error, loadedCollection) {
-		collection = loadedCollection;
-	});
+  connection.collection('article', function(error, loadedCollection) {
+    collection = loadedCollection;
+  });
 
-	var
-		crudDelegate,
-		entityDelegate = Entity.createEntityDefinition(schema);
+  var
+    crudDelegate,
+    entityDelegate = Entity.createEntityDefinition(schema);
 
-	entityDelegate.schema = schema;
+  entityDelegate.schema = schema;
 
-	crudDelegate = MongodbCrudDelegate.createMongodbCrudDelegate(
-		'Article',
-		'Articles',
-		'_id',
-		collection,
-		entityDelegate,
-		MongodbCrudDelegate.objectIdFilter(connection),
-		serviceLocator.logger
-	);
+  crudDelegate = MongodbCrudDelegate.createMongodbCrudDelegate(
+    'Article',
+    'Articles',
+    '_id',
+    collection,
+    entityDelegate,
+    MongodbCrudDelegate.objectIdFilter(connection),
+    serviceLocator.logger
+  );
 
-	function createRss(callback) {
+  function createRss(callback) {
 
-		var feed = new RSS({
-			title: 'Clock Blog',
-			description: 'Clock Blog',
-			feed_url: url.resolve(properties.siteUrl, '/feed'),
-			site_url: properties.siteUrl,
-			image_url: url.resolve(properties.siteUrl, '/favicon.ico'),
-			author: 'Clock Ltd'
-		});
+    var feed = new RSS({
+      title: 'Clock Blog',
+      description: 'Clock Blog',
+      feed_url: url.resolve(properties.siteUrl, '/feed'),
+      site_url: properties.siteUrl,
+      image_url: url.resolve(properties.siteUrl, '/favicon.ico'),
+      author: 'Clock Ltd'
+    });
 
-		crudDelegate.findWithUrl({}, { sort: { publishedDate: -1 } }, function(error, dataSet) {
-			if (error) {
-				return callback(error);
-			}
-			dataSet.forEach(function(article) {
-				feed.item({
-					title: article.title,
-					description: article.body,
-					url: article.url,
-					author: article.author,
-					date: article.publishedDate
-				});
-			});
+    crudDelegate.findWithUrl({}, { sort: { publishedDate: -1 } }, function(error, dataSet) {
+      if (error) {
+        return callback(error);
+      }
+      dataSet.forEach(function(article) {
+        feed.item({
+          title: article.title,
+          description: article.body,
+          url: article.url,
+          author: article.author,
+          date: article.publishedDate
+        });
+      });
 
-			callback(null, feed.xml());
-		});
-	}
+      callback(null, feed.xml());
+    });
+  }
 
-	function findWithUrl(query, options, callback) {
+  function findWithUrl(query, options, callback) {
 
-		// Ensuring all queries shown to the front end are live
-		if (typeof query.live === 'undefined') {
-			query.live = true;
-		}
+    // Ensuring all queries shown to the front end are live
+    if (typeof query.live === 'undefined') {
+      query.live = true;
+    }
 
-		crudDelegate.find(query, options, function(error, articles) {
-			if (error) {
-				return callback(error);
-			}
-			articles.forEach(function(article) {
-				//Constructing the url of the page for fb like and G+ buttons
-				article.path = path.join('/', article.section, article.slug);
-				article.url = url.resolve(
-					properties.siteUrl,
-					path.join(
-						article.section,
-						article.slug
-					)
-				);
-			});
-			callback(error, articles);
-		});
-	}
+    crudDelegate.find(query, options, function(error, articles) {
+      if (error) {
+        return callback(error);
+      }
+      articles.forEach(function(article) {
+        //Constructing the url of the page for fb like and G+ buttons
+        article.path = path.join('/', article.section, article.slug);
+        article.url = url.resolve(
+          properties.siteUrl,
+          path.join(
+            article.section,
+            article.slug
+          )
+        );
+      });
+      callback(error, articles);
+    });
+  }
 
-	crudDelegate.pipes.beforeCreate.add(function(entity, callback) {
-		callback(null, entityDelegate.makeDefault(entity));
-	});
+  crudDelegate.pipes.beforeCreate.add(function(entity, callback) {
+    callback(null, entityDelegate.makeDefault(entity));
+  });
 
-	crudDelegate.findWithUrl = findWithUrl;
-	crudDelegate.createRss = createRss;
+  crudDelegate.findWithUrl = findWithUrl;
+  crudDelegate.createRss = createRss;
 
-	return crudDelegate;
+  return crudDelegate;
 };
--- bundles/articleAdmin/controller.js
+++ bundles/articleAdmin/controller.js
@@ -1,120 +1,120 @@
 var
-	_ = require('underscore'),
-	formHelper = require('../../lib/utils/formHelper'),
-	generic = require('../generic'),
-	adminController = require('./adminController'),
-	viewRenderDelegate = require('../../lib/viewRenderDelegate');
+  _ = require('underscore'),
+  formHelper = require('../../lib/utils/formHelper'),
+  generic = require('../generic'),
+  adminController = require('./adminController'),
+  viewRenderDelegate = require('../../lib/viewRenderDelegate');
 
 module.exports.createRoutes = function(app, properties, serviceLocator, bundleViewPath) {
 
-	var viewSchema = generic.createViewSchema({
-		groups: [{
-			name: 'Article Details',
-			description: 'These are the details for an Article',
-			properties: {
-				title: {
-					list: true,
-					view: true,
-					searchType: 'text'
-				},
-				section: {
-					list: true,
-					view: true
-				},
-				slug: {
-					list: false,
-					view: true,
-					searchType: 'text'
-				},
-				summary: {
-					view: true,
-					searchType: 'text'
-				},
-				author: {
-					list: true,
-					view: true,
-					searchType: 'text'
-				},
-				live: {
-					list: true,
-					view: true
-				},
-				comments: {
-					view: true
-				},
-				created: {
-					list: false,
-					view: true,
-					type: 'dateTime'
-				},
-				publishedDate: {
-					list: true,
-					view: true,
-					edit: true,
-					type: 'dateTime'
-				}
-			}
-		},
-		{
-			name: 'Article Content',
-			description: 'This is the content for an Article',
-			properties: {
-				type: {
-					view: true
-				},
-				body: {
-					view: true
-				}
-			}
-		},
-		{
-			name: 'Article Images',
-			description: 'Images for an Article',
-			properties: {
-				images: {
-					view: true,
-					type: 'file'
-				}
-			}
-		}],
-		title: 'title',
-		formPostHelper: function(req, res, next) {
-			var proc = formHelper.processors;
+  var viewSchema = generic.createViewSchema({
+    groups: [{
+      name: 'Article Details',
+      description: 'These are the details for an Article',
+      properties: {
+        title: {
+          list: true,
+          view: true,
+          searchType: 'text'
+        },
+        section: {
+          list: true,
+          view: true
+        },
+        slug: {
+          list: false,
+          view: true,
+          searchType: 'text'
+        },
+        summary: {
+          view: true,
+          searchType: 'text'
+        },
+        author: {
+          list: true,
+          view: true,
+          searchType: 'text'
+        },
+        live: {
+          list: true,
+          view: true
+        },
+        comments: {
+          view: true
+        },
+        created: {
+          list: false,
+          view: true,
+          type: 'dateTime'
+        },
+        publishedDate: {
+          list: true,
+          view: true,
+          edit: true,
+          type: 'dateTime'
+        }
+      }
+    },
+    {
+      name: 'Article Content',
+      description: 'This is the content for an Article',
+      properties: {
+        type: {
+          view: true
+        },
+        body: {
+          view: true
+        }
+      }
+    },
+    {
+      name: 'Article Images',
+      description: 'Images for an Article',
+      properties: {
+        images: {
+          view: true,
+          type: 'file'
+        }
+      }
+    }],
+    title: 'title',
+    formPostHelper: function(req, res, next) {
+      var proc = formHelper.processors;
 
-			formHelper.process(req, {
-				images: proc.file,
-				live: proc.boolean,
-				comments: proc.boolean,
-				publishedDate: proc.date,
-				removeImage: proc.removeImage(['images'])
-			});
+      formHelper.process(req, {
+        images: proc.file,
+        live: proc.boolean,
+        comments: proc.boolean,
+        publishedDate: proc.date,
+        removeImage: proc.removeImage(['images'])
+      });
 
-			next();
-		}
-	});
+      next();
+    }
+  });
 
-	var articleViewRender = viewRenderDelegate.create(__dirname + '/views');
+  var articleViewRender = viewRenderDelegate.create(__dirname + '/views');
 
-	// These routes are here to override the default generic form
-	adminController.createRoutes(
-		app,
-		articleViewRender,
-		viewSchema,
-		serviceLocator.articleModel,
-		serviceLocator,
-		{
-			requiredAccess: 'Article'
-		}
-	);
+  // These routes are here to override the default generic form
+  adminController.createRoutes(
+    app,
+    articleViewRender,
+    viewSchema,
+    serviceLocator.articleModel,
+    serviceLocator,
+    {
+      requiredAccess: 'Article'
+    }
+  );
 
-	generic.createRoutes(
-		app,
-		generic.createViewRender('../../admin/views/layout'),
-		viewSchema,
-		serviceLocator.articleModel,
-		serviceLocator,
-		{
-			requiredAccess: 'Article'
-		}
-	);
+  generic.createRoutes(
+    app,
+    generic.createViewRender('../../admin/views/layout'),
+    viewSchema,
+    serviceLocator.articleModel,
+    serviceLocator,
+    {
+      requiredAccess: 'Article'
+    }
+  );
 };
