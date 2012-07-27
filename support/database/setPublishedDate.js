f3922fc565987b3ac920dd9900978b070f6b2aec
Paul Serby <paul.serby@clock.co.uk>
2012-07-27 23:40:13 +0100

    Removing unused

--- support/database/setPublishedDate.js
+++
@@ -1,4 +0,0 @@
-db.article.find().forEach(function(article) {
-  article.publishedDate = article.created;
-  db.article.save(article);
-});
--- support/tabtospace.sh
+++
@@ -1,5 +0,0 @@
-files=`find . -type f -name "*.js" -o -name "*.jade" -o -name "*.json"`
-for file in $files
-do
-  expand -t 2 "$file" > /tmp/expandtmp && mv /tmp/expandtmp "$file"
-done
