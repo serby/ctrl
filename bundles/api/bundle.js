4a1d1cb95cd0fd6d0ddf37c53f5c5730f21481ac
Erik Lundin <mjor.himself@gmail.com>
2012-08-09 12:47:20 +0100

    First draft of the REST API generating bundle

---
+++ bundles/api/bundle.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'API',
+  version: '0.0.1',
+  description: 'Generate RESTful APIs from a defined Entity schema',
+  initialize: function(serviceLocator, done) {
+    serviceLocator.register('buildApi', require('./'));
+    done();
+  }
+};
---
+++ bundles/api/test/test-api-generation.js
@@ -0,0 +1,233 @@
+var http = require('http')
+  , express = require('express')
+  , request = require('request')
+  , assert = require('assert')
+  , generateApi = require('../');
+
+
+var mockModel = {
+  find: function(query, options) {
+    assert.deepEqual(query, {});
+
+    return { toArray: function(callback) {
+      callback(null, [
+        { id: 3, colour: 'blue' },
+        { id: 7, colour: 'red' }
+      ]);
+    } };
+  },
+  findOne: function(id, callback) {
+    assert.equal(id, 3);
+    callback(null, { id: 3, colour: 'blue' });
+  },
+  create: function(query, options, callback) {
+    assert.deepEqual(query, { colour: 'yellow' });
+    callback(null, { id: 9, colour: 'yellow' });
+  },
+  update: function(id, changes, options, callback) {
+    if (id === 7 || id === '7') {
+      callback(null, { id: 7, colour: 'purple' });
+    } else {
+      assert.equal(id, 2);
+      assert.deepEqual(changes, { colour: 'black' });
+      callback(null);
+    }
+  },
+  'delete': function(id, callback) {
+    assert.equal(id, 9);
+    callback();
+  }
+};
+
+var mockErrorModel = {
+  find: function(query, options) {
+    return { toArray: function(callback) {
+      callback(new Error(''));
+    } };
+  },
+  findOne: function(query, callback) {
+    callback(new Error(''));
+  },
+  create: function(query, options, callback) {
+    callback(new Error(''));
+  },
+  update: function(id, changes, options, callback) {
+    callback(new Error(''));
+  },
+  'delete': function(id, callback) {
+    callback(new Error('delete'));
+  }
+};
+
+
+describe('generated api', function() {
+
+  var port = 6430
+    , prefix = 'http://127.0.0.1:' + port
+    , app;
+
+
+  beforeEach(function(done) {
+    app = express.createServer();
+    app.use(express.bodyParser());
+    app.listen(port, done);
+  });
+
+  afterEach(function() {
+    app.close();
+    prefix = 'http://127.0.0.1:' + (++port);
+  });
+
+
+  it('should list all resources on "GET /"', function(done) {
+    generateApi(app, mockModel, { base: '/api' });
+
+    request.get(prefix + '/api', function(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 200);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.deepEqual(JSON.parse(body), { error: null, result: [
+        { id: 3, colour: 'blue' },
+        { id: 7, colour: 'red' }
+      ]});
+      done();
+    });
+  });
+
+  it('display a specific resource on "GET /id"', function(done) {
+    generateApi(app, mockModel, { base: '/api' });
+
+    request.get(prefix + '/api/3', function(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 200);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.deepEqual(JSON.parse(body), { error: null, result: { id: 3, colour: 'blue' } });
+      done();
+    });
+  });
+
+  it('should create a resource on "POST /"', function(done) {
+    generateApi(app, mockModel, { base: '/api' });
+
+    request.post(prefix + '/api', { form: { colour: 'yellow' } }, function(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 200);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.deepEqual(JSON.parse(body), { error: null, result: { id: 9, colour: 'yellow' } });
+      done();
+    });
+  });
+
+  it('should update a resource on "PUT /id"', function(done) {
+    generateApi(app, mockModel, { base: '/api' });
+
+    request.put(prefix + '/api/7', { form: { colour: 'purple' } }, function(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 200);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.deepEqual(JSON.parse(body), { error: null, result: { id: 7, colour: 'purple' } });
+      done();
+    });
+  });
+
+  it('should not allow PUT requests to unexisting IDs', function(done) {
+    generateApi(app, mockModel, { base: '/api' });
+
+    request.put(prefix + '/api/2', { form: { colour: 'black' } }, function(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 404);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.deepEqual(JSON.parse(body), { error: 'Not Found' });
+      done();
+    });
+  });
+
+  it('should delete a resource on "DELETE /id"', function(done) {
+    generateApi(app, mockModel, { base: '/api' });
+
+    request.del(prefix + '/api/9', function(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 200);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.deepEqual(JSON.parse(body), { error: null });
+      done();
+    });
+  });
+
+  it('should reply with 405 for disallowed methods', function(done) {
+    generateApi(app, mockModel, {
+      'base':   '/api',
+      'list':   false,
+      'read':   false,
+      'create': false,
+      'update': false,
+      'delete': false
+    });
+
+    var n = 0;
+
+    function step(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 405);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.deepEqual(JSON.parse(body), { error: "Method Not Allowed" });
+
+      if (++n === 5) {
+        done();
+      }
+    }
+
+    request.get(prefix + '/api', step);
+    request.post(prefix + '/api', step);
+    request.get(prefix + '/api/3', step);
+    request.put(prefix + '/api/3', step);
+    request.del(prefix + '/api/3', step);
+  });
+
+  it('should hide error messages by default', function(done) {
+    generateApi(app, mockErrorModel, { base: '/api' });
+
+    var n = 0;
+
+    function step(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 500);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.deepEqual(JSON.parse(body), { error: 'Internal Server Error' });
+
+      if (++n === 5) {
+        done();
+      }
+    }
+
+    request.get(prefix + '/api', step);
+    request.post(prefix + '/api', { form: { colour: 'yellow' } }, step);
+    request.get(prefix + '/api/3', step);
+    request.put(prefix + '/api/7', { form: { colour: 'purple' } }, step);
+    request.del(prefix + '/api/9', step);
+  });
+
+  it('should display error messages when enabled', function(done) {
+    generateApi(app, mockErrorModel, { base: '/api', explain: true });
+
+    var n = 0;
+
+    function step(err, res, body) {
+      assert.equal(err, null);
+      assert.equal(res.statusCode, 500);
+      assert(/^application\/json/.test(res.headers['content-type']));
+      assert.notDeepEqual(JSON.parse(body), { error: 'Internal Server Error' });
+
+      if (++n === 5) {
+        done();
+      }
+    }
+
+    request.get(prefix + '/api', step);
+    request.post(prefix + '/api', { form: { colour: 'yellow' } }, step);
+    request.get(prefix + '/api/3', step);
+    request.put(prefix + '/api/7', { form: { colour: 'purple' } }, step);
+    request.del(prefix + '/api/9', step);
+  });
+
+});
