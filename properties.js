c187557836156bf538dca3e127df1222f3c383b9
Paul Serby <paul.serby@clock.co.uk>
2012-10-13 22:08:35 +0100

    Formatting

--- properties.js
+++ properties.js
@@ -1,6 +1,5 @@
 var _ = require('lodash')
   , basePort = 3020
-  ;
 
 var properties = {
     version: '0.1.0'
@@ -24,41 +23,41 @@ var properties = {
   , bcryptWorkFactor: 1
   , debug: true
   , changeEmailSender: 'nobody@example.com'
-};
+}
 
 var environmentProperties = {
     development: {}
   , testing: {
     siteUrl: 'http://localhost:' + (basePort + 2)
-    , database: {
-      replSet:
-      { name: 'ctrl'
-      , servers: [
-        { host: 'localhost', port: 28000 }
-        , { host: 'localhost', port: 28001 }
-        ]
+    , database:
+      { replSet:
+        { name: 'ctrl'
+        , servers: [
+          { host: 'localhost', port: 28000 }
+          , { host: 'localhost', port: 28001 }
+          ]
+        }
+      , name: 'ctrl-testing'
       }
-      , name: 'ctrl-Testing'
-    }
   }
   , production: {
     siteUrl: 'http://localhost:' + (basePort + 3)
     , port: basePort + 3
     , email: 'paul.serby@clock.co.uk'
     , database:
-    { host: '127.0.0.1'
-    , port: 27017
-    , name: 'ctrl-Production'
-    }
+      { host: '127.0.0.1'
+      , port: 27017
+      , name: 'ctrl-production'
+      }
   }
-};
+}
 
 module.exports = function getProperties (environment) {
 
-  properties.env = environment = environment || process.env.NODE_ENV || 'development';
+  properties.env = environment = environment || process.env.NODE_ENV || 'development'
 
   if (environmentProperties[environment] === undefined) {
-    throw new RangeError('No properties for environment \'' + environment + '\'');
+    throw new RangeError('No properties for environment \'' + environment + '\'')
   }
-  return _.extend({}, properties, environmentProperties[environment]);
-};
+  return _.extend({}, properties, environmentProperties[environment])
+}
