15c559933bbc5eafa5917be2e6bbc5c8f92a8548
Adam Duncan <adam.duncan@clock.co.uk>
2012-11-15 20:48:51 +0000

    Fixed missing logger level

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function log() {
 // bundles need all of these.
 serviceLocator
   .register('properties', properties)
-  .register('logger', { info: log, debug: log, warn: log, error: log, verbose: log })
+  .register('logger', { info: log, debug: log, warn: log, error: log, verbose: log, silly: log })
   .register('mailer', nodemailer.createTransport('sendmail'))
   .register('saveFactory', {})
 
